0f9bd0d90ee7939f536d2074743a4a74587f6c91
Claudio Acioli <kllaudyo@gmail.com>
2019-07-27 20:28:50 -0300

    feat: Implementa form wtf

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from hello import NameForm
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -12,11 +13,22 @@ moment = Moment(app)
 app.config['SECRET_KEY'] = 'Hard to guees string'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     user_agent = request.headers.get('User-Agent')
     print(user_agent)
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template(
+        'index.html',
+        current_time=datetime.utcnow(),
+        form=form,
+        name=name
+    )
 
 
 @app.route('/user/<name>')
