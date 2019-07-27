17913cf08196cf3bb1ad7de1762607af392f9b41
Claudio Acioli <kllaudyo@gmail.com>
2019-07-27 16:41:56 -0300

    feat: Adiciona rota

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}</h1>'.format(name)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
