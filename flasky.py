600626dd2e70fd718e259d17fbbedef8ff8ec79f
Claudio Acioli <kllaudyo@gmail.com>
2019-07-28 16:09:53 -0300

    feat: Adiciona comando cli de test

--- flasky.py
+++ flasky.py
@@ -11,6 +11,13 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+@app.cli.command()
+def test():
+    """Exceuta testes"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
