5b41e1b7a6281447422d1beb46394e73ff7a4dcc
Claudio Acioli <kllaudyo@gmail.com>
2019-07-28 12:55:11 -0300

    chore: Remove arquivos nao utilizados

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class NameForm(FlaskForm):
-    name = StringField('Qual é o seu nome:', validators=[DataRequired()])
-    submit = SubmitField('Submit')
