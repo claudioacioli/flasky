8ef08651a042dc4db2bf170dc5dab967265b50d9
Claudio Acioli <kllaudyo@gmail.com>
2019-08-03 00:45:43 -0300

    fix: Corrige contexto da aplicacao para thread

--- app/email.py
+++ app/email.py
@@ -10,10 +10,14 @@ def send_async_email(app, msg):
 
 
 def send_email(to, subject, template, **kwargs):
+    # Codigo adicionado mais nao estudado
+    # Apos verificar erro no envio de emails analise o codigo do autor do livro
+    # Ele adicionou esta linha, talvez nao tenha percebido o pq no decorrer do livro
+    app = current_app._get_current_object()
     msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[current_app, msg])
+    thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
