154c8066afb3011004315c57b118d81d1bae6787
Claudio Acioli <kllaudyo@gmail.com>
2019-08-17 20:42:40 -0300

    feat: Cria formulario

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
     name = StringField('Qual é o seu nome:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Nome:', validators=[Length(0,64)])
+    location = StringField('Localização: ', validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
