b6e9cf1c0315efd42a6931b701211ae52b1b3ef6
Claudio Acioli <kllaudyo@gmail.com>
2019-08-17 18:46:53 -0300

    feat: Injeta classe permissao em templates

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,3 +3,11 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+
+@main.app_context_processor
+def inject_permission():
+    """Injeta dicionario de permissoes nos templates da main"""
+    return dict(Permission=Permission)
+
