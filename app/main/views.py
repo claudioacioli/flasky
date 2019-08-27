9240e84d5219ccf5a0420189c199f11471aee293
Claudio Acioli <kllaudyo@gmail.com>
2019-08-27 06:22:30 -0300

    feat: Adiciona controle de submit

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 
-from flask import \
-    render_template,\
-    session,\
-    redirect,\
-    url_for,\
-    current_app
-from flask_login import login_required, current_user    
+from flask import (
+    render_template,
+    session,
+    redirect,
+    url_for,
+    current_app,
+    flash
+)
+from flask_login import login_required, current_user
 from . import main as app_main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 from .. import db
-from ..models import User
+from ..models import User, Role
 from ..email import send_email
 from ..decorators import admin_required
 
@@ -71,4 +73,22 @@ def edit_profile():
 def edit_profile_admin(id):
     user = User.query.get_or_404(id)
     form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('Perfil atualizado!')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.confirmed
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
