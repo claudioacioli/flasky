25baf2f425f6876a74f3b19e689f4f3f735b2687
Claudio Acioli <kllaudyo@gmail.com>
2019-08-27 06:21:22 -0300

    refactor: Altera variaveis de ambiente

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in [
         'true', 'on', '1'
     ]
-    MAIL_USERNAME = os.environ.get('FLASKY_MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('FLASKY_MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@flasky.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
