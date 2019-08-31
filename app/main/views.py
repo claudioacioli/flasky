ce0355a54f790f623eb3b08d2c5836257ea5f1fc
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-08-30 22:41:20 -0300

    feat: Busca posts do banco de dados e envia para template

--- app/main/views.py
+++ app/main/views.py
@@ -36,7 +36,10 @@ def index():
 @app_main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    if user is None:
+        abort(404)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @app_main.route('/edit-profile', methods=['GET','POST'])
