4a3500bab1301387f7573a1fa2c28d831500e095
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-04 22:00:07 -0300

    feat: Adiciona controle de comentarios na rota

--- app/main/views.py
+++ app/main/views.py
@@ -10,9 +10,9 @@ from flask import (
 )
 from flask_login import login_required, current_user
 from . import main as app_main
-from .forms import EditProfileForm, EditProfileAdminForm, PostForm
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from .. import db
-from ..models import User, Role, Permission, Post
+from ..models import User, Role, Permission, Post, Comment
 from ..decorators import admin_required, permission_required
 
 
@@ -120,10 +120,39 @@ def edit_profile_admin(id):
     return render_template('edit_profile.html', form=form, user=user)
 
 
-@app_main.route('/posts/<int:id>', methods=['GET'])
+@app_main.route('/posts/<int:id>', methods=['GET', 'POST'])
 def post(id):
     post = Post.query.get_or_404(id)
-    return render_template('post.html', posts=[post])
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(
+            body=form.body.data,
+            post=post,
+            author=current_user._get_current_object()
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been published.')
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+
+    if page == -1:
+        page = (post.comments.count() - 1) \
+               // current_app.config['FLASKY_POSTS_PER_PAGE'] + 1
+
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page,
+        per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False
+    )
+    comments = pagination.items
+    return render_template(
+        'post.html',
+        posts=[post],
+        comments=comments,
+        pagination=pagination,
+        form=form
+    )
 
 
 @app_main.route('/edit/<int:id>', methods=['GET', 'POST'])
