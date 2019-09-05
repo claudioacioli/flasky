75ea8941a99fa6c9249e386351aaecec23b0a3f4
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-04 22:39:09 -0300

    feat: Adiciona variavel configuracao

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = 10
+    FLASKY_COMMENTS_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
