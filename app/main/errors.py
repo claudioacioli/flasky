9666c42ada79090acf38eba186eba9eb9633c1fc
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-06 07:09:21 -0300

    feat: Retorna erro 403 em formato apropriado

--- app/main/errors.py
+++ app/main/errors.py
@@ -24,5 +24,10 @@ def internal_server_error(e):
 
 @app_main.app_errorhandler(403)
 def forbiden(e):
+    if request.accept_mimetypes.accept_json and\
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'forbidden'})
+        response.status_code = 403
+        return response
     return render_template('403.html'), 403
 
