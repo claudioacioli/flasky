940ee8665f4048f3178381c8fb6168b3dced759d
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-06 07:11:19 -0300

    refactor: Reordena erros e altera mensagens

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,6 +2,16 @@ from flask import request, jsonify, render_template
 from . import main as app_main
 
 
+@app_main.app_errorhandler(403)
+def forbiden(e):
+    if request.accept_mimetypes.accept_json and\
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('403.html'), 403
+
+
 @app_main.app_errorhandler(404)
 def page_not_found(e):
     if request.accept_mimetypes.accept_json and\
@@ -16,18 +26,7 @@ def page_not_found(e):
 def internal_server_error(e):
     if request.accept_mimetypes.accept_json and\
             not request.accept_mimetypes.accept_html:
-        response = jsonify({'error':'Server error'})
+        response = jsonify({'error':'internal server error'})
         response.status_code = 500
         return response
     return render_template('500.html'), 500
-
-
-@app_main.app_errorhandler(403)
-def forbiden(e):
-    if request.accept_mimetypes.accept_json and\
-            not request.accept_mimetypes.accept_html:
-        response = jsonify({'error':'forbidden'})
-        response.status_code = 403
-        return response
-    return render_template('403.html'), 403
-
