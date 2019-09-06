88d132dfc0a08ac867f00ab8d6175c7cb25e2431
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-06 07:07:57 -0300

    feat: Retorna erro 500 em formato apropriado

--- app/main/errors.py
+++ app/main/errors.py
@@ -14,6 +14,11 @@ def page_not_found(e):
 
 @app_main.app_errorhandler(500)
 def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and\
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'Server error'})
+        response.status_code = 500
+        return response
     return render_template('500.html'), 500
 
 
