f21e210cd11e4475bbc2057f286384970032b793
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-06 21:37:51 -0300

    feat: Adiciona gerador/leitor token

--- app/models.py
+++ app/models.py
@@ -206,6 +206,19 @@ class User(UserMixin, db.Model):
     def followed_posts(self):
         return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     @staticmethod
     def add_self_follows():
         for user in User.query.all():
