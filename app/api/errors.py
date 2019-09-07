81f6c6673cc38f743a0f537f1a24f20f13540fb2
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-06 22:34:11 -0300

    feat: Adiciona tratamento para Exceção no programa

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,5 +1,6 @@
 from flask import jsonify
-
+from . import api as app_api
+from ..exceptions import ValidationError
 
 def bad_request(message):
     response = jsonify({'error': 'not found', 'message': message})
@@ -17,3 +18,8 @@ def forbidden(message):
     response = jsonify({'error': 'forbidden', 'message': message})
     response.status_code = 403
     return response
+
+
+@app_api.errorhandler(ValidationError)
+def validation_error(e):
+    return bad_request(e.args[0])
