0b4c088d854ee23c3805723f8956b8d38a6483d8
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-14 19:03:28 -0300

    feat: Cria rota para comentarios de um determinado post

--- app/api/posts.py
+++ app/api/posts.py
@@ -3,7 +3,7 @@ from . import api as app_api
 from .errors import forbidden
 from .decorators import permission_required
 from .. import db
-from ..models import Post, Permission
+from ..models import Post, Comment, Permission
 
 
 @app_api.route('/posts/', methods=['POST'])
@@ -43,6 +43,26 @@ def get_posts():
 def get_post(id):
     post = Post.query.get_or_404(id)
     return jsonify(post.to_json())
+
+
+@app_api.route('/posts/<int:id>/comments/')
+def get_post_comments(id):
+    post = Post.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = post.comments.paginate(
+        page,
+        per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False
+    )
+    comments = pagination.items
+    prev = url_for('api.get_post_comments', page=page - 1) if pagination.has_prev else None
+    next = url_for('api.get_post_comments', page=page + 1) if pagination.has_next else None
+    return jsonify({
+        'comments': [comment.to_json() for comment in comments],
+        'prev_url': prev,
+        'next_url': next,
+        'count': pagination.total
+    })
 
 
 @app_api.route('/posts/<int:id>', methods=['PUT'])
