6fd609298ee0153d3f6b8e1df21d0cd79361c5b1
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-14 20:09:56 -0300

    feat: Adiciona rota

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import authentication, errors, decorators, users, posts
+from . import authentication, errors, decorators, users, posts, comments
