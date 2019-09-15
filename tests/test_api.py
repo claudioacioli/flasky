2765031dc1e0c617625b5582c456526bce6f9f0e
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-15 00:06:50 -0300

    test: Adiciona teste para api

---
+++ tests/test_api.py
@@ -0,0 +1,34 @@
+import re
+from base64 import b64encode
+import unittest
+from flask import url_for
+from app import create_app, db
+from app.models import User, Role
+
+
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        # use_cookies é setado para false pois API's Rest não mantém estado.
+        self.client = self.app.test_client(use_cookies=False)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def get_api_headers(self, username, password):
+        return {
+            'Authorization':
+                'Basic ' + b64encode((username + ':' + password).encode('utf-8')).decode('utf-8'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+    def test_no_auth(self):
+        response = self.client.get('/api/v1/posts/', content_type='application/json')
+        self.assertEqual(response.status_code, 401)
