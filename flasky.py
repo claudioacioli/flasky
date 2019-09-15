bfcdd64af814dea43133b77098e3bc282e7a7a9c
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-14 22:31:44 -0300

    feat: Adiciona flag para cobertura

--- flasky.py
+++ flasky.py
@@ -1,4 +1,13 @@
 import os
+import sys
+import click
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
 from app.models import User, Role, Permission
 from flask_migrate import Migrate
@@ -18,11 +27,28 @@ def make_shell_context():
 
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False, help='Executa testes de cobertura')
+def test(coverage):
     """Exceuta testes"""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + [x + '.exe' if 'flask' in x else x for x in sys.argv])
+        # Codigo abaixo nao funcionou no windows por conta do .exe.
+        # Todo(1) Verifiar uma solução robusta
+        # os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Resumo de cobertura: ')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
