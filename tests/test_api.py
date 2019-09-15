bd0dda0d69d24d80cd1d1fae68fc19986a0a5567
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-15 00:20:32 -0300

    tests: Adiciona teste postagem api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,6 @@
-import re
+import json
 from base64 import b64encode
 import unittest
-from flask import url_for
 from app import create_app, db
 from app.models import User, Role
 
@@ -32,3 +31,29 @@ class APITestCase(unittest.TestCase):
     def test_no_auth(self):
         response = self.client.get('/api/v1/posts/', content_type='application/json')
         self.assertEqual(response.status_code, 401)
+
+    def test_posts(self):
+        # adiciona um usuario
+        r = Role.query.filter_by(name='User').first()
+        self.assertIsNotNone(r)
+        u = User(email='john@example.com', password='cat', confirmed=True, role=r)
+        db.session.add(u)
+        db.session.commit()
+
+        # escreve postagem
+        response = self.client.post(
+            '/api/v1/posts/',
+            headers=self.get_api_headers('john@example.com','cat'),
+            data=json.dumps({'body': 'Criando com os *testes*'})
+        )
+        self.assertEqual(response.status_code, 201)
+        url = response.headers.get('Location')
+        self.assertIsNotNone(url)
+
+        # Obtem postagem
+        response = self.client.get(url, headers=self.get_api_headers('john@example.com','cat'))
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual('http://localhost' + json_response['url'], url)
+        self.assertEqual(json_response['body'], 'Criando com os *testes*')
+        self.assertEqual(json_response['body_html'], '<p>Criando com os <em>testes</em></p>')
