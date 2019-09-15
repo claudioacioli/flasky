a8376cbb98abdf98f69160082a1bb2084f7229e7
Claudio Acioli <claudio.acioli.silva@gmail.com>
2019-09-14 23:35:34 -0300

    test: Adiciona teste para criacao/login usuario

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 from app import create_app, db
 from app.models import User, Role
@@ -21,3 +22,21 @@ class FlaskClientTestCase(unittest.TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue('Stranger' in response.get_data(as_text=True))
+
+    def test_register_and_login(self):
+        # Cadastra uma nova conta!
+        response = self.client.post('/auth/register', data={
+            'email': 'john@example.com',
+            'username': 'john',
+            'password': 'cat',
+            'confirm_password': 'cat'
+        })
+        self.assertEqual(response.status_code, 302)
+        # Faz login
+        response = self.client.post('/auth/login', data={
+            'email': 'john@example.com',
+            'password': 'cat'
+        }, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(re.search('Hello,\s+john!', response.get_data(as_text=True)))
+        self.assertTrue('Voce ainda nao confirmou sua conta!' in response.get_data(as_text=True))
